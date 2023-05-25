a5fb86f2be045767ca959aaa7c016d16012049b3
samuelvalverdee <savalverde@estudiantec.cr>
2023-05-24 21:55:24 -0600

    display burger

--- Cocina/src/cocina/Burger.java
+++ Cocina/src/cocina/Burger.java
@@ -10,14 +10,23 @@ public class Burger {
 	String salsa;
 	ArrayList<String> ingredientes = new ArrayList<String>();
 	
-	public Burger() {
-		
-	}
-	
 	public Burger(ArrayList<String> specs) {
 		
 		this.ingredientes = specs;
 		
+	}
+	
+	public String toString() {
+
+		StringBuffer display = new StringBuffer();
+		display.append("---- " + nombre + " ----\n");
+		display.append(torta + "\n");
+		display.append(queso + "\n");
+		display.append(salsa + "\n");
+		for (String i : ingredientes) {
+			display.append(i + "\n");
+		}
+		return display.toString();
 	}
 
 }
